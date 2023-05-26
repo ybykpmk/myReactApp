d91d9802a7ee76e9b7792c2929259e8c4fa3bb5c
Test Kullanicisi <test@gmail.com>
2023-05-26 20:17:48 +0300

    save/update product function is written

--- src/redux/actions/productActions.js
+++ src/redux/actions/productActions.js
@@ -4,6 +4,25 @@ export function getProductsSuccess(products) {
     return { type: actionTypes.GET_PRODUCTS_SUCCESS, payload: products }
 }
 
+export function createProductSuccess(product) {
+    return { type: actionTypes.CREATE_PRODUCT_SUCCESS, payload: product }
+}
+
+export function updateProductSuccess(product) {
+    return { type: actionTypes.UPDATE_PRODUCT_SUCCESS, payload: product }
+}
+
+export function saveProduct(product) {
+    return fetch("http://localhost:3000/products/" + (product.id || ""),
+        {
+            method: product.id ? "PUT" : "POST",
+            headers: { "content-type": "application/json" },
+            body: JSON.stringify(product)
+        })
+        .then(handleResponse)
+        .catch(handleError);
+}
+
 export function getProducts(categoryId) {
     return function (dispatch) {
         let url = "http://localhost:3000/products";
