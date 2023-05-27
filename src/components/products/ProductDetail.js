8db54f817445912693b3295cf5f10f89f5a190a0
Test Kullanicisi <test@gmail.com>
2023-05-28 00:05:20 +0300

    save/update product function is being written5

--- src/components/products/ProductDetail.js
+++ src/components/products/ProductDetail.js
@@ -1,17 +1,25 @@
 import React from "react";
 import TextInput from '../toolbox/TextInput';
 import { Button } from "reactstrap";
+import SelectInput from "../toolbox/SelectInput";
 
-const ProductDetail=(
+const ProductDetail = ({
     categories,
     product,
     onSave,
     onChange
-)=>{
-    return(
+}) => {
+    return (
         <form onSubmit={onSave}>
-            <h2>{product.id?"Güncelle":"Ekle"}</h2>
+            <h2>{product.id ? "Güncelle" : "Ekle"}</h2>
             <TextInput name="productName" label="Product Name" value={product.productName} onChange={onChange} error="Hata"></TextInput>
+            <SelectInput name="categoryId" label="Category" value={product.categoryId || ""} defaultOption="Seçiniz" options={categories.map(category => ({
+                value: category.id,
+                text: category.categoryName
+            }))} 
+            onChange={onChange}
+            error="Hata"
+            />
             <Button type="submit" className="btn btn-success">Save</Button>
         </form>
     )
